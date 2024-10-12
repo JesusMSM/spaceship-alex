6ceac93f249cd909c550fc2f76ec7ef25e1e5ef0
Alex Tellez <alextellezyanes@gmail.com>
2024-10-12 13:03:23 +0200

    completed ItemController class

--- Spaceship/Spaceship/src/main/java/com/example/Spaceship/Controllers/ItemController.java
+++ Spaceship/Spaceship/src/main/java/com/example/Spaceship/Controllers/ItemController.java
@@ -1,5 +1,27 @@
 package com.example.Spaceship.Controllers;
 
-public class ItemController {
+import java.util.List;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.Spaceship.Models.Item;
+
+@RestController
+@RequestMapping(path = "api/v1/item")
+public class ItemController {
+    //Define un endpoint HTTP GET que devuelve una lista de artículos (Item)
+//en formato JSON cuando se accede a ese endpoint
+	@GetMapping
+        public List<Item> getItems() {
+        return List.of(
+          new Item(
+                  1L,
+                  "Machines",
+                   150.50,
+                    "Equipment"
+		    )
+		  );
+    }
 }
